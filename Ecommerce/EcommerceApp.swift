2b0257ecb2a529cfb3945f983b412e2b417218e6
andrii <andriy.2130duda@gmail.com>
2026-01-26 21:36:23 +0200

    Initial Commit

---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,32 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by Andrii Duda on 26.01.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct EcommerceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
